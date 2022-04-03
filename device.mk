f3b23cf7b943ae5ff0f843b134303ea8721b8f1b
alexandrusemina <alexandrusemina8@gmail.com>
2022-04-04 00:12:29 +0300

    fixed ram usage and app crashing

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ DERP_BUILDTYPE := Official
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.exynos7885
+    android.hardware.usb@1.3-service.samsung
 
 TARGET_SCREEN_HEIGHT := 1560
 TARGET_SCREEN_WIDTH := 720
@@ -21,8 +21,7 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-   fstab.exynos7884B \
-   ueventd.exynos7885.rc
+   fstab.exynos7884B
    
 PRODUCT_PACKAGES += \
    android.hardware.sensors@1.0-service
